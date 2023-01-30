573fcb8bef1dd9a86a9f708a632227953b8c92a1
Rodolfo Andrés Rivas Matta <rodolforivasmatta@gmail.com>
2023-01-30 16:55:52 -0500

    Added test script.
    Added script and files used for testing the automator.

---
+++ tests/test_pdf_automator.py
@@ -0,0 +1,10 @@
+# Testing the functionalities of the PDF Automator class.
+import sys
+sys.path.append("../")
+from resplit import PDF_Automator
+
+intput_dir = "input_dir"
+output_dir = "output_dir"
+
+automator = PDF_Automator(intput_dir, output_dir)
+automator.perform("selection", {"pages": "1, 3-5"})
